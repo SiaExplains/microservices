98f4da33c6ef2ba3d30203511bb43d21385022c3
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-05-24 14:58:24 +0200

    complete app.js

--- articles/src/app.js
+++ articles/src/app.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const Article = require('./models/article.model');
 
-console.log('Microservice!');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'articles' });
+});
+
+app.get('/api/v1/articles', async (req, res) => {
+    const articles = await Article.find({});
+    res.json(articles);
+});
+
+app.post('/api/v1/articles', async (req, res) => {
+    const article = new Article({ name: req.body.name });
+    const savedArticle = await article.save();
+    res.json(savedArticle);
+});
+
+module.exports = app;
