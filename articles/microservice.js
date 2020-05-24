3f69c9e6cb21ba137f568b5cd3ebe9b98f714fdb
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-05-24 15:00:26 +0200

    micro-service connect to mongodb via mongoose

--- articles/microservice.js
+++ articles/microservice.js
@@ -1,2 +1,11 @@
-const app = require('./src/app');
 const mongoose = require('mongoose');
+
+const app = require('./src/app');
+const { DB_URI } = require('./src/config');
+
+mongoose.connect(DB_URI);
+
+app.listen(3000, () => {
+    console.log('article microservice is running on port 3000');
+    console.log('+--------------------------+');
+});
